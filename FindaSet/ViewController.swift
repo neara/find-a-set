5571d744a917e380d9e1fb53fb87834a4b44d888
anatr <ana@tapingo.com>
2015-05-09 15:08:36 +0300

    Initial Commit

---
+++ FindaSet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindaSet
+//
+//  Created by Ana Trakhtman on 5/9/15.
+//  Copyright (c) 2015 Ana Trakhtman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
